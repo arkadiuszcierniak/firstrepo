323f3b82f0c158c78b1c10a28ea518bf539a3cc3
Arkadiusz Cierniak <arkadiusz.cierniak@student.put.poznan.pl>
2018-01-10 09:13:44 +0100

    commit 1

---
+++ widget.cpp
@@ -0,0 +1,34 @@
+#include "widget.h"
+#include "ui_widget.h"
+
+Widget::Widget(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
+void Widget::on_findButton_clicked()
+{
+
+}
+
+void TextFinder::loadTextFile()
+{
+    QFile inputFile(":/input.txt");
+    inputFile.open(QIODevice::ReadOnly);
+
+    QTextStream in(&inputFile);
+    QString line = in.readAll();
+    inputFile.close();
+
+    ui->textEdit->setPlainText(line);
+    QTextCursor cursor = ui->textEdit->textCursor();
+    cursor.movePosition(QTextCursor::Start, QTextCursor::MoveAnchor, 1);
+
+}
